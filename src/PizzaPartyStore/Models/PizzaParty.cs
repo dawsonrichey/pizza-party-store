0c5dafcc7f2b58cb7db580675dc5ce9690ca1151
Dawson Richey <dawsonrichey@gmail.com>
2018-07-22 23:00:39 -0400

    Added pizza party and topping models

---
+++ src/PizzaPartyStore/Models/PizzaParty.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PizzaPartyStore.Models
+{
+	public class PizzaParty
+	{
+		public int Id { get; set; }
+		public string PizzaName { get; set; }
+		public int PizzaCost { get; set; }
+		public string DescriptionHtml { get; set; }
+		public Topping[] Toppings { get; set; }
+		public bool Favorite { get; set; }
+	}
+}
