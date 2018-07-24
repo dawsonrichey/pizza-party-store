c94537362055868c5475e91ec33ffc9366a6ee9e
Dawson Richey <dawsonrichey@gmail.com>
2018-07-24 06:57:05 -0400

    Updated the default route

--- src/PizzaPartyStore/App_Start/RouteConfig.cs
+++ src/PizzaPartyStore/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace PizzaPartyStore
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "PizzaParty", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
