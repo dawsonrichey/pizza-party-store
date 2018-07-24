e62c78b795066c438048c3ebe8a547f9792d3826
Dawson Richey <dawsonrichey@gmail.com>
2018-07-24 19:57:44 -0400

    Initialized the enity framework project model

---
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Models/PizzaParty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaPartyStoreModel.Models
+{
+	public class PizzaParty
+	{
+		//ID, PizzaPartsId, PizzaPartyID
+		public int Id { get; set; }
+		public string PizzaName { get; set; }
+		public int PizzaCost { get; set; }
+		public string Description { get; set; }
+		public DateTime PublishedOn { get; set; }
+
+	}
+}
