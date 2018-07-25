3822153ad37dbecc8cc933327956fd79373b7795
Dawson Richey <dawsonrichey@gmail.com>
2018-07-24 21:02:30 -0400

    Completed using the context

--- src/PizzaPartyStoreModel/PizzaPartyStoreModel/Program.cs
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PizzaPartyStoreModel.Models;
 
 namespace PizzaPartyStoreModel
 {
@@ -10,6 +11,25 @@ namespace PizzaPartyStoreModel
 	{
 		static void Main(string[] args)
 		{
+			using (var context = new Context())
+			{
+				context.PizzaParties.Add(new PizzaParty()
+				{
+					PizzaName = "fart",
+					PizzaCost = 1,
+					PublishedOn = DateTime.Today
+				});
+				context.SaveChanges();
+
+				var pizzaParties = context.PizzaParties.ToList();
+				foreach (var pizzaParty in pizzaParties)
+				{
+					Console.WriteLine(pizzaParty.PizzaName);
+				}
+
+				Console.ReadLine();
+			}
+
 		}
 	}
 }
