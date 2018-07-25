d1affcbdd497ebe205016ef34dba8be337bde729
Dawson Richey <dawsonrichey@gmail.com>
2018-07-24 20:05:31 -0400

    Completed  adding our context

---
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PizzaPartyStoreModel.Models;
+
+namespace PizzaPartyStoreModel
+{
+	public class Context : DbContext
+	{
+		public DbSet<PizzaParty> PizzaParties { get; set; }
+
+	}
+}
