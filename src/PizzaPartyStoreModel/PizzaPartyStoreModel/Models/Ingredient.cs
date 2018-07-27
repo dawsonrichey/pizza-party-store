354634253be3193dabfca70cee755811b07fb4a0
Dawson Richey <dawsonrichey@gmail.com>
2018-07-26 21:19:45 -0400

    Completed using data annotations to refine the generated database

--- src/PizzaPartyStoreModel/PizzaPartyStoreModel/Models/Ingredient.cs
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Models/Ingredient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +14,7 @@ namespace PizzaPartyStoreModel.Models
 			PizzaParties = new List<PizzaPartyIngredient>();
 		}
 		public int Id { get; set; }
+		[Required, StringLength(100)]
 		public string Name { get; set; }
 
 		public ICollection<PizzaPartyIngredient> PizzaParties { get; set; }
--- src/PizzaPartyStoreModel/PizzaPartyStoreModel/Models/Pizza.cs
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Models/Pizza.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace PizzaPartyStoreModel.Models
 		}
 
 		public int Id { get; set; }
+		[Required, StringLength(200)]
 		public string Name { get; set; }
 		public string Description { get; set; }
 
