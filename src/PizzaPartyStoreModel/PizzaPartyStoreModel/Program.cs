957f942cbe7f96ac99906ae4b3f6a6aad10203cf
Dawson Richey <dawsonrichey@gmail.com>
2018-07-26 22:19:36 -0400

    Completed list queries section

--- src/PizzaPartyStoreModel/PizzaPartyStoreModel/Program.cs
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using PizzaPartyStoreModel.Models;
 using System.Data.Entity;
+using System.Diagnostics;
 
 namespace PizzaPartyStoreModel
 {
@@ -14,23 +15,30 @@ namespace PizzaPartyStoreModel
 		{
 			using (var context = new Context())
 			{
-				
+				context.Database.Log = (message) => Debug.WriteLine(message);
 
-				var pizzaParties = context.PizzaParties
-					.Include(pp => pp.Pizza)
-					.Include(pp => pp.Ingredients.Select(a => a.Ingredient))
-					.Include(pp => pp.Ingredients.Select(a => a.Role))
-					.ToList();
+				//var pizzaParties = context.PizzaParties.ToList();
 
-				foreach (var pizzaParty in pizzaParties)
-				{
-					var ingredientRoleNames = pizzaParty.Ingredients
-						.Select(a => $"{a.Ingredient.Name} - {a.Role.Name}").ToList();
-					var ingredientsRolesDisplayText = string.Join(", ", ingredientRoleNames);
+				var pizzaPartiesQuery = from pp in context.PizzaParties select pp;
+				var pizzaParties = pizzaPartiesQuery.ToList();
 
-					Console.WriteLine(pizzaParty.DisplayText);
-					Console.WriteLine(ingredientsRolesDisplayText);
-				}
+				Console.WriteLine("# of pizza party pizzas: {0}", pizzaParties.Count);
+
+				//var pizzaParties = context.PizzaParties
+				//	.Include(pp => pp.Pizza)
+				//	.Include(pp => pp.Ingredients.Select(a => a.Ingredient))
+				//	.Include(pp => pp.Ingredients.Select(a => a.Role))
+				//	.ToList();
+
+				//foreach (var pizzaParty in pizzaParties)
+				//{
+				//	var ingredientRoleNames = pizzaParty.Ingredients
+				//		.Select(a => $"{a.Ingredient.Name} - {a.Role.Name}").ToList();
+				//	var ingredientsRolesDisplayText = string.Join(", ", ingredientRoleNames);
+
+				//	Console.WriteLine(pizzaParty.DisplayText);
+				//	Console.WriteLine(ingredientsRolesDisplayText);
+				//}
 
 				Console.ReadLine();
 			}
