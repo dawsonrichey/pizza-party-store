ec4c4b4c82810941f90c7be9ad88372255fc0dfd
Dawson Richey <dawsonrichey@gmail.com>
2018-07-26 21:36:20 -0400

    Completed Overriding the context's OnModelCreating Method

--- src/PizzaPartyStoreModel/PizzaPartyStoreModel/Context.cs
+++ src/PizzaPartyStoreModel/PizzaPartyStoreModel/Context.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,18 @@ namespace PizzaPartyStoreModel
 		}
 
 		public DbSet<PizzaParty> PizzaParties { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+			//modelBuilder.Conventions.Remove<DecimalPropertyConvention>();
+			//modelBuilder.Conventions.Add(new DecimalPropertyConvention(5, 2));
+
+			modelBuilder.Entity<PizzaParty>()
+				.Property(pp => pp.AverageRating)
+				.HasPrecision(5, 2);
+		}
 
 	}
 }
